9cc2eb607bcbf255f353b49df87a294f5328d441
codewarnab <dp950810@gmail.com>
2024-10-09 13:11:19 +0530

    vc

--- src/app/calls/[room]/page.tsx
+++ src/app/calls/[room]/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
+import { MeetingRoom } from '@/components/meeting-room';
+import { NavigationBar } from '@/components/NaviGationBar';
 import { FC } from 'react';
 
 const Room: FC<{ params: { room: string } }> = ({ params: {
     room
 } }) => {
     return (
-        <div>Room :{room} </div>
+        <>
+            <NavigationBar
+                trigger={null}
+            />
+            <MeetingRoom
+            chatId={room}
+            />
+        </>
     )
 }
 
---
+++ src/app/calls/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { NavigationBar } from '@/components/NaviGationBar'
+import React from 'react'
+
+const page = () => {
+  return (
+      <>
+          <NavigationBar
+          trigger={null}
+          
+          />
+      </>
+  )
+}
+
+export default page 
