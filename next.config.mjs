531bf7a75e651467fad26ebfbab941c9ec219075
codewarnab <dp950810@gmail.com>
2024-10-09 16:39:17 +0530

    ignore ts

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,12 @@ const nextConfig = {
             },
         ],
     },
+    typescript: {
+        ignoreBuildErrors: true, // Ignore TypeScript errors during build
+    },
+    eslint: {
+        ignoreDuringBuilds: true, // Ignore ESLint errors during build
+    },
 };
 
 export default nextConfig;
